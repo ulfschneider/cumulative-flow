a39825a84d1aef03b92cfe55894d6b20f4308ed8
Schneider, Ulf <ulf.schneider@dieboldnixdorf.com>
2018-08-30 16:11:39 +0200

    Initial entry point

---
+++ cfd.js
@@ -0,0 +1,16 @@
+'use strict'
+var d3 = require('d3');
+var moment = require('moment');
+
+function CFD() {
+
+}
+
+CFD.prototype.draw = function(data, options) {
+    var cfd
+    return cfd;
+}
+
+CFD[Symbol.species] = CFD;
+
+module.exports = cfd;
