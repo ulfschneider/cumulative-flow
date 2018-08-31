f47259b18f1b3c4bf2120440433a7461234bd62b
Schneider, Ulf <ulf.schneider@dieboldnixdorf.com>
2018-08-31 15:24:07 +0200

    Working jsdom with empty svg

--- cfd.js
+++ cfd.js
@@ -1,11 +1,13 @@
 'use strict'
 
 var jsdom = require('jsdom');
-var {JSDOM} = jsdom;
+var {
+    JSDOM
+} = jsdom;
 var d3 = require('d3');
 var moment = require('moment');
 
-function CFD(settings) {    
+function CFD(settings) {
     this.settings = settings;
 }
 
@@ -15,42 +17,60 @@ CFD.prototype.validateSettings = function() {
     var self = this;
 
     if (!self.settings) {
-       throw "No settings"
+        throw "No settings"
     }
 
     if (!self.settings.margin) {
-        self.settings.margin = {top:0, right:0, bottom:0, left:0};
+        self.settings.margin = {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        };
     } else {
         var marginKeys = ['top', 'right', 'bottom', 'left'];
         marginKeys.forEach(function(key) {
-            if (! (key in self.settings.margin)) {
+            if (!(key in self.settings.margin)) {
                 margin[key] = 0;
             }
         });
     }
 
-    if (! ('width' in self.settings)) {
+    if (!('width' in self.settings)) {
         self.settings.width = 600;
     }
     self.settings.innerWidth = self.settings.width - self.settings.margin.left - self.settings.margin.right;
-    
-    if (! ('height' in self.settings)) {
-        self.settings.height = 400;        
-    }        
+
+    if (!('height' in self.settings)) {
+        self.settings.height = 400;
+    }
     self.settings.innerHeight = self.settings.height - self.settings.margin.top - self.settings.margin.left;
 }
 
-CFD.prototype.draw = function()  {
+CFD.prototype.draw = function() {
     var self = this;
     self.validateSettings();
 
-    var dom = new JSDOM('svg');
-    var domElement = d3.select(dom.body);
-    self.svg = domElement.append('svg');
-    self.g = self.svg.append("g")
-                .attr("transform", "translate(" + self.settings.margin.left + "," + self.settings.margin.top + ")");
+
+
+
+    var dom = new JSDOM();
+    var d3Element = d3.select(dom.window.document.body);
+
+    self.svg = d3Element.append('svg')
+        .attr('xmlns', 'http://www.w3.org/2000/svg')
+        .attr('width', self.settings.width)
+        .attr('height', self.settings.height);
+
+    self.g = self.svg.append("g");
+    if (self.settings.margin.left || self.settings.margin.top) {
+        g.attr("transform", "translate(" + self.settings.margin.left + "," + self.settings.margin.top + ")");
+    }
+
+    console.log(dom.serialize());
+    console.log(dom.window.document.documentElement.outerHTML);
 
     return self.svg;
 }
- 
+
 module.exports = CFD;
--- cfd.test.js
+++ cfd.test.js
@@ -13,21 +13,30 @@ test('validate settings', () => {
 
     //no settings at all
     var cfd = new CFD();
-    expect(() => cfd.draw()).toThrow(/No settings/);
+    expect(() => cfd.draw())
+        .toThrow(/No settings/);
 
     //empty settings
     var settings = {};
     cfd = new CFD(settings);
-    
+
     //no exception expected for empty settings
     cfd.draw();
-    
-    expect(settings.width).toBe(600);
-    expect(settings.height).toBe(400);
-    expect(settings.innerWidth).toBe(600);
-    expect(settings.innerHeight).toBe(400);
-    expect(settings.margin.top).toBe(0);
-    expect(settings.margin.right).toBe(0);
-    expect(settings.margin.bottom).toBe(0);
-    expect(settings.margin.left).toBe(0);  
+
+    expect(settings.width)
+        .toBe(600);
+    expect(settings.height)
+        .toBe(400);
+    expect(settings.innerWidth)
+        .toBe(600);
+    expect(settings.innerHeight)
+        .toBe(400);
+    expect(settings.margin.top)
+        .toBe(0);
+    expect(settings.margin.right)
+        .toBe(0);
+    expect(settings.margin.bottom)
+        .toBe(0);
+    expect(settings.margin.left)
+        .toBe(0);
 });
