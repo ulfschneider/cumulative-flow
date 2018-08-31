4b79e78e99ea2738f879ed684ddabb07188b277e
Schneider, Ulf <ulf.schneider@dieboldnixdorf.com>
2018-08-31 16:29:31 +0200

    Prepare data checks

--- cfd.js
+++ cfd.js
@@ -7,10 +7,17 @@ var {
 var d3 = require('d3');
 var moment = require('moment');
 
+//Helper functions
+
 function validateSettings(settings) {
     if (!settings) {
-        throw "No settings"
+        throw "No settings";
     }
+    //TODO check for data
+    //TODO check for data is array with suitable size
+    //TODO check for existence of columns, fromData, toData
+    //TODO check for doneState, indeterminateState, newState
+
     prepareMargins(settings);
 }
 
@@ -43,11 +50,66 @@ function prepareMargins(settings) {
 
 }
 
-function prepareScales(settings) {
+function prepareSVG(settings) {
+    settings.dom = JSDOM.fragment('<svg></svg>');
+    settings.svg = d3.select(settings.dom.firstChild);
 
+    settings.svg
+        .attr('xmlns', 'http://www.w3.org/2000/svg')
+        .attr('width', settings.width)
+        .attr('height', settings.height);
+
+    var g = settings.svg.append("g");
+    if (settings.margin.left || settings.margin.top) {
+        g.attr("transform", "translate(" + settings.margin.left + "," + settings.margin.top + ")");
+    }
+}
+
+function prepareScales(settings) {
+    settings.x = d3.scaleTime()
+        .range([0, settings.width]);
+    settings.y = d3.scaleLinear()
+        .range([settings.height, 0]);
+}
+
+function prepareDataFunctions(settings) {
+
+    settings.stack = d3.stack();
+    settings.area = d3.area()
+        .x(function(d, i) {
+            return settings.x(d.data.date);
+        })
+        .y0(function(d) {
+            return settings.y(d[0]);
+        })
+        .y1(function(d) {
+            return settings.y(d[1]);
+        });
+
+    var xRange = d3.extent(settings.data, function(d) {
+        return d.date;
+    });
+    if (settings.fromDate) {
+        xRange[0] = settings.fromDate;
+    }
+    if (settings.toDate) {
+        xRange[1] = settings.toDate;
+    }
+    settings.x.domain(xRange);
+    settings.keys = settings.data.columns.slice(1);
+    settings.stack.keys(settings.keys);
+    settings.y.domain([0, d3.max(settings.data, function(d) {
+        var sum = 0;
+        for (var i = 1, n = settings.data.columns.length; i < n; i++) {
+            sum += d[settings.data.columns[i]];
+        }
+        return sum;
+    })]);
 }
 
 
+
+//Object with API
 
 function CFD(settings) {
     this.settings = settings;
@@ -55,26 +117,19 @@ function CFD(settings) {
 
 CFD[Symbol.species] = CFD;
 
-CFD.prototype.draw = function() {
+CFD.prototype.draw = function(settings) {
     var self = this;
-    validateSettings(self.settings);
-
-    var dom = JSDOM.fragment('<svg></svg>');
-    self.svg = d3.select(dom.firstChild);
-
-    self.svg
-        .attr('xmlns', 'http://www.w3.org/2000/svg')
-        .attr('width', self.settings.width)
-        .attr('height', self.settings.height);
-
-    self.g = self.svg.append("g");
-    if (self.settings.margin.left || self.settings.margin.top) {
-        g.attr("transform", "translate(" + self.settings.margin.left + "," + self.settings.margin.top + ")");
+    if (settings) {
+        self.settings = settings;
     }
+    validateSettings(self.settings);
+    prepareSVG(self.settings);
+    prepareScales(self.settings);
+    //TODO after checks for data, call prepareDataFunctions(self.settings);
 
-    console.log(dom.firstChild.outerHTML);
+    console.log(self.settings.dom.firstChild.outerHTML);
 
-    return dom.firstChild.outerHTML;
+    return self.settings.dom.firstChild.outerHTML;
 }
 
 module.exports = CFD;
