feff6703cb5305f3aea6aac25400fce008d5b019
Schneider, Ulf <ulf.schneider@dieboldnixdorf.com>
2018-09-06 11:02:09 +0200

    Draw cfd areas and axis

--- cfd.js
+++ cfd.js
@@ -7,19 +7,20 @@ var {
 var d3 = require('d3');
 var moment = require('moment');
 
+
+const FONT_SIZE = '16';
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 400;
+
 //Helper functions
 
 function validateSettings(settings) {
     if (!settings) {
         throw "No settings";
     }
-    //TODO check for data
-    //TODO check for data is array with suitable size
-    //TODO check for existence of columns, fromData, toData
-    //TODO check for doneState, indeterminateState, newState
 
     validateData(settings);
-    prepareMargins(settings);
+    validateMargins(settings);
 }
 
 function validateData(settings) {
@@ -37,17 +38,17 @@ function validateData(settings) {
 
 }
 
-function prepareMargins(settings) {
+function validateMargins(settings) {
     if (!settings.margin) {
         settings.margin = {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
+            top: 75,
+            right: 210,
+            bottom: 30,
+            left: 80
         };
     } else {
         var marginKeys = ['top', 'right', 'bottom', 'left'];
-        marginKeys.forEach(function(key) {
+        marginKeys.forEach(function (key) {
             if (!(key in settings.margin)) {
                 margin[key] = 0;
             }
@@ -55,14 +56,14 @@ function prepareMargins(settings) {
     }
 
     if (!('width' in settings)) {
-        settings.width = 600;
+        settings.width = DEFAULT_WIDTH;
     }
     settings.innerWidth = settings.width - settings.margin.left - settings.margin.right;
 
     if (!('height' in settings)) {
-        settings.height = 400;
+        settings.height = DEFAULT_HEIGHT;
     }
-    settings.innerHeight = settings.height - settings.margin.top - settings.margin.left;
+    settings.innerHeight = settings.height - settings.margin.top - settings.margin.bottom;
 
 }
 
@@ -75,34 +76,35 @@ function prepareSVG(settings) {
         .attr('width', settings.width)
         .attr('height', settings.height);
 
-    var g = settings.svg.append("g");
+    settings.g = settings.svg.append("g");
     if (settings.margin.left || settings.margin.top) {
-        g.attr("transform", "translate(" + settings.margin.left + "," + settings.margin.top + ")");
+        settings.g.attr("transform", "translate(" + settings.margin.left + "," + settings.margin.top + ")");
     }
 }
 
 function prepareScales(settings) {
     settings.x = d3.scaleTime()
-        .range([0, settings.width]);
+        .range([0, settings.innerWidth]);
     settings.y = d3.scaleLinear()
-        .range([settings.height, 0]);
+        .range([settings.innerHeight, 0]);
 }
 
 function prepareDataFunctions(settings) {
 
     settings.stack = d3.stack();
     settings.area = d3.area()
-        .x(function(d, i) {
+        //.curve(d3.curveStepAfter) - this kind of interpolation is more correct, but not very readable
+        .x(function (d, i) {
             return settings.x(d.data.date);
         })
-        .y0(function(d) {
+        .y0(function (d) {
             return settings.y(d[0]);
         })
-        .y1(function(d) {
+        .y1(function (d) {
             return settings.y(d[1]);
         });
 
-    var xRange = d3.extent(settings.data, function(d) {
+    var xRange = d3.extent(settings.data, function (d) {
         return d.date;
     });
     if (settings.fromDate) {
@@ -112,28 +114,143 @@ function prepareDataFunctions(settings) {
         xRange[1] = settings.toDate;
     }
     settings.x.domain(xRange);
-    settings.keys = settings.data.columns.slice(1);
+    //settings.keys = settings.data.columns.slice(1);
+    settings.keys = settings.data.done;
+    settings.keys = settings.keys.concat(settings.data.progress);
+    settings.keys = settings.keys.concat(settings.data.toDo);
     settings.stack.keys(settings.keys);
-    settings.y.domain([0, d3.max(settings.data, function(d) {
+    settings.y.domain([0, d3.max(settings.data, function (d) {
         var sum = 0;
-        for (var i = 1, n = settings.data.columns.length; i < n; i++) {
-            sum += d[settings.data.columns[i]];
+        for (var i = 0, n = settings.keys.length; i < n; i++) {
+            sum += d[settings.keys[i]];
         }
         return sum;
     })]);
 }
 
 
+function isProgressStatus(status, settings) {
+    return settings.data.progress.indexOf(status) >= 0;
+}
+
+function isDoneStatus(status, settings) {
+    return settings.data.done.indexOf(status) >= 0;
+}
+
+
+function drawAxis(settings) {
+    settings.axisStyle = {
+        color: '#000'
+    }
+    var xAxis = settings.g.append('g')
+        .attr('transform', 'translate(0,' + settings.innerHeight + ')')
+        .call(d3.axisBottom(settings.x));
+    xAxis
+        .selectAll('path')
+        .style('stroke', settings.axisStyle.color);
+    xAxis
+        .selectAll('line')
+        .style('stroke', settings.axisStyle.color);
+    xAxis
+        .selectAll('text')
+        .style('fill', settings.axisStyle.color)
+        .style('font', FONT_SIZE + 'px sans-serif');
+
+    var yAxis = settings.g.append('g')
+        .attr('transform', 'translate(' + settings.innerWidth + ' ,0)')
+        .call(d3.axisRight(settings.y));
+    yAxis
+        .selectAll('path')
+        .style('stroke', settings.axisStyle.color);
+    yAxis
+        .selectAll('line')
+        .style('stroke', settings.axisStyle.color);
+    yAxis
+        .selectAll('text')
+        .style('fill', settings.axisStyle.color)
+        .style('font', FONT_SIZE + 'px sans-serif');
+}
+
+function drawLayers(settings) {
+    settings.toDoStyle = {
+        fill: '#bec0c2',
+        stroke: '#fff',
+        color: '#bec0c2'
+    };
+    settings.progressStyle = {
+        fill: '#808285',
+        stroke: '#fff',
+        color: '#808285'
+    };
+    settings.doneStyle = {
+        fill: '#000',
+        stroke: '#fff',
+        color: '#000'
+    };
+
+    let layer = settings.g.selectAll('.layer')
+        .data(settings.stack(settings.data))
+        .enter()
+        .append('g')
+        .attr('class', 'layer');
+
+    layer.append('path')
+        .attr('class', 'area')
+        .style('fill', function (d) {
+            if (isProgressStatus(d.key, settings)) {
+                return settings.progressStyle.fill;
+            } else if (isDoneStatus(d.key, settings)) {
+                return settings.doneStyle.fill;
+            }
+            return settings.toDoStyle.fill;
+        })
+        .style('stroke', function (d) {
+            if (isProgressStatus(d.key, settings)) {
+                return settings.progressStyle.stroke;
+            } else if (isDoneStatus(d.key, settings)) {
+                return settings.doneStyle.stroke;
+            }
+            return settings.toDoStyle.stroke;
+        })
+        .style('stroke-width', '.5')
+        .attr('d', settings.area);
+
+    layer.filter(function (d) {
+            return settings.data.length && settings.y(d[d.length - 1][0]) - settings.y(d[d.length - 1][1]) >= FONT_SIZE;
+        })
+        .append('text')
+        .attr('x', settings.width + 50)
+        .attr('y', function (d) {
+            return settings.y(d[d.length - 1][1]);
+        })
+        .attr('dy', '.35em')
+        .style('font', FONT_SIZE + 'px sans-serif')
+        .style('text-anchor', 'start')
+        .style('fill', function (d) {
+            if (isProgressStatus(d.key, settings)) {
+                return settings.progressStyle.color;
+            } else if (isDoneStatus(d.key, settings)) {
+                return settings.doneStyle.color;
+            }
+            return settings.toDoStyle.color;
+        })
+        .text(function (d) {
+            return (d[d.length - 1][1] - d[d.length - 1][0]) + ' ' + d.key;
+        });
+}
+
 
 //Object with API
 
 function CFD(settings) {
     this.settings = settings;
+    this.defaultWidth = DEFAULT_WIDTH;
+    this.defaultHeight = DEFAULT_HEIGHT;
 }
 
 CFD[Symbol.species] = CFD;
 
-CFD.prototype.draw = function(settings) {
+CFD.prototype.draw = function (settings) {
     var self = this;
     if (settings) {
         self.settings = settings;
@@ -143,7 +260,8 @@ CFD.prototype.draw = function(settings) {
     prepareScales(self.settings);
     prepareDataFunctions(self.settings);
 
-    console.log(self.settings.dom.firstChild.outerHTML);
+    drawLayers(self.settings);
+    drawAxis(self.settings);
 
     return self.settings.dom.firstChild;
 }
--- cfd.test.js
+++ cfd.test.js
@@ -1,23 +1,31 @@
 'use strict'
 
+var fs = require('fs');
+
 var CFD = require('./cfd');
 var moment = require('moment');
-
+var data = makeTestData();
 
 // helper functions
 function makeTestData() {
     var testData = [];
     var now = moment();
     testData.columns = ['new', 'dev', 'test', 'done'];
-    testData.todo = ['new'];
+    testData.toDo = ['new'];
     testData.progress = ['dev', 'test'];
     testData.done = ['done'];
     testData.push({
+            date: moment(now).subtract(8, 'days'),
+            new: 0,
+            dev: 0,
+            test: 0,
+            done: 0,
+        }, {
             date: moment(now).subtract(7, 'days'),
             new: 1,
             dev: 0,
             test: 0,
-            done: 0
+            done: 0,
         }, {
             date: moment(now).subtract(6, 'days'),
             new: 1,
@@ -60,6 +68,10 @@ function makeTestData() {
     return testData;
 }
 
+function writeTestFile(path, content) {
+    fs.writeFile(path, content);
+}
+
 
 //test the functions
 test('species', () => {
@@ -94,25 +106,26 @@ test('validate settings', () => {
     }).toThrow(/Data is not an array/);
 
 
-    settings.data = [];
-
-    cfd.draw();
+    settings.data = data;
+    var domTree = cfd.draw();
+    var testFileContent = '<!DOCTYPE html>\n<meta charset="utf-8">\n' + domTree.outerHTML;
+    writeTestFile('./cfd.html', testFileContent);
 
     //now the defaults must be set
     expect(settings.width)
-        .toBe(600);
+        .toBe(cfd.defaultWidth);
     expect(settings.height)
-        .toBe(400);
+        .toBe(cfd.defaultHeight);
     expect(settings.innerWidth)
-        .toBe(600);
+        .toBe(cfd.defaultWidth - settings.margin.left - settings.margin.right);
     expect(settings.innerHeight)
-        .toBe(400);
+        .toBe(cfd.defaultHeight - settings.margin.top - settings.margin.bottom);
     expect(settings.margin.top)
-        .toBe(0);
+        .toBe(75);
     expect(settings.margin.right)
-        .toBe(0);
+        .toBe(210);
     expect(settings.margin.bottom)
-        .toBe(0);
+        .toBe(30);
     expect(settings.margin.left)
-        .toBe(0);
+        .toBe(80);
 });
